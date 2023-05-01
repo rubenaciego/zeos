2c53be6a0bb42c470c8fb1009a6969ca6c2c850f
Sergi Soler 'fraret <fraretblauet@gmail.com>
2023-05-01 18:51:05 +0200

    Threads implemented

--- user.c
+++ user.c
@@ -1,9 +1,24 @@
 #include <libc.h>
 
+void thread_func(void* param) {
+  char* m = "\nThread\n";
+  write(1, m, strlen(m));
+  volatile time_loss = 0;
+  for (int i = 0; i < 60000000; ++i) {
+      ++time_loss;
+    }
+  char* s = "\nThread2\n";
+  write(1, s, strlen(s));
+}
+
+
 int __attribute__ ((__section__(".text.main")))
   main(void)
 {
   char buff[256];
+  
+  create_thread(thread_func, 0);
+  
   while (1)
   {
     volatile int time_loss = 0;
