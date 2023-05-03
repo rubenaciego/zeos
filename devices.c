1d650c06a40af969067a29f581da4eebf412b1cb
ruben.aciego <ruben.aciego@estudiantat.upc.edu>
2023-05-03 19:03:52 +0200

    terminal codes

--- devices.c
+++ devices.c
@@ -12,9 +12,57 @@ int sys_write_console(char *buffer, int size)
   for (int i = 0; i < size; ++i)
   {
     // check terminal codes
-    if (buffer[i] == esc)
+    if (buffer[i] == esc && size - i > 1 && buffer[i+1] == '[')
     {
+      // foreground color [ 3 color m
+      // background color [ 4 color m
+      if (size - i >= 5 && buffer[i+4] == 'm')
+      {
+        char fg_bg = buffer[i+2];
+        char color = buffer[i+3];
 
+        if (color >= '0' && color <= '7')
+        {
+          Byte col = color - '0';
+          if (fg_bg == '3')
+          {
+            screen.palette = (screen.palette & 0xf0) | col;
+            i += 4;
+          }
+          else if (fg_bg == '4')
+          {
+            screen.palette = (screen.palette & 0x0f) | (col << 4);
+            i += 4;
+          }
+        }
+      }
+      // move_cursor [ xxx ; yyy H
+      else if (size - i >= 10 && buffer[i+5] == ';' && buffer[i+9] == 'H')
+      {
+        int x = 0;
+        int y = 0;
+
+        for (int j = 0; j < 3; ++j)
+        {
+          char cx = buffer[i+j+2];
+          char cy = buffer[i+j+6];
+          if (cx < '0' || cx > '9' || cy < '0' || cy  > '9')
+          {
+            x = y = -1;
+            break;
+          }
+
+          x = 10 * x + cx - '0';
+          y = 10 * y + cy - '0';
+        }
+
+        if (x >= 0 && x < NUM_COLUMNS && y >= 0 && y < NUM_ROWS)
+        {
+          screen.x = (Byte)x;
+          screen.y = (Byte)y;
+          i += 9;
+        }
+      }
     }
     else
       printc(buffer[i]);
--- user.c
+++ user.c
@@ -6,10 +6,63 @@ void remove_last()
   write(1, &msg, 2);
 }
 
+void move_cursor(unsigned char x, unsigned char y)
+{
+  static char buff[10] = {0x1b, '[', 'x', 'x', 'x', ';', 'y', 'y', 'y', 'H'};
+
+  for (int i = 0; i < 3; ++i)
+  {
+    buff[4 - i] = (x % 10) + '0';
+    buff[8 - i] = (y % 10) + '0';
+    x /= 10;
+    y /= 10;
+  }
+
+  write(1, buff, 10);
+}
+
+typedef enum
+{
+  BLACK = 0,
+  BLUE,
+  GREEN,
+  CYAN,
+  RED,
+  MAGENTA,
+  BROWN,
+  GRAY
+} color_t;
+
+void set_fg_color(color_t color)
+{
+  static char buff[5] = {0x1b, '[', '3', 'x', 'm'};
+
+  if (color <= 7)
+  {
+    buff[3] = color + '0';
+    write(1, buff, 5);
+  }
+}
+
+void set_bg_color(color_t color)
+{
+  static char buff[5] = {0x1b, '[', '4', 'x', 'm'};
+
+  if (color <= 7)
+  {
+    buff[3] = color + '0';
+    write(1, buff, 5);
+  }
+}
+
 int __attribute__ ((__section__(".text.main")))
   main(void)
 {
   char buff[256];
+
+  move_cursor(0, 10);
+  set_fg_color(BLUE);
+  set_bg_color(RED);
 
   buff[0] = 'A';
   write(1, buff, 1);
